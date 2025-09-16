4f88af1668a398705f06e771233ed821e75dfa5a
Didula Ratnayake <it24101577@my.sliit.lk>
2025-09-16 20:37:42 +0530

    Added Lab 07

---
+++ Lab Sheet 07/IT24101577.R
@@ -0,0 +1,14 @@
+setwd("/Users/dede/Desktop/Lab07")
+getwd()
+# Q1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+# Q2
+pexp(2, rate = 1/3)
+
+# Q3 (i)
+1 - pnorm(130, mean = 100, sd = 15)
+
+# Q3 (ii)
+qnorm(0.95, mean = 100, sd = 15)
+
