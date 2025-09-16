a48f42313dbe552a8586bf60bcc51659d9849ea3
Didula Ratnayake <it24101577@my.sliit.lk>
2025-09-16 10:51:19 +0530

    added lab 6

---
+++ Lab Sheet 06/IT24101577.R
@@ -0,0 +1,18 @@
+#1
+setwd("C:\\Users\\it24101577\\Desktop\\IT24101577\\Lab06")
+#i
+#Binomial Distribution
+
+#ii
+1 - pbinom(46, 50, 0.85, lower.tail = TRUE)
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+#2
+#i
+# Number of customer calls received per hour
+
+#ii
+# Poisson distribution
+
+#iii
+dpois(15, 12)
